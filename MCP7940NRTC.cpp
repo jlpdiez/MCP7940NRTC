f83cbe67c3b63cd7672122b75f07cb5659fd5354
Melkoroth <ender.vs.melkor@gmail.com>
2019-04-05 20:58:59 +0200

    Moar stuff

--- MCP7940NRTC.cpp
+++ MCP7940NRTC.cpp
@@ -1,34 +1,13 @@
-/*
- * MCP7940NRTC.h - library for MCP7940N RTC
-  
-  Copyright (c) Juan L. Perez Diez 2019
-  This library is intended to be uses with Arduino Time library functions
-
-  The library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-  1 Apr 2019 - Initial release
- */
-
-
 #if defined (__AVR_ATtiny84__) || defined(__AVR_ATtiny85__) || (__AVR_ATtiny2313__)
-#include <TinyWireM.h>
-#define Wire TinyWireM
+    #include <TinyWireM.h>
+    #define Wire TinyWireM
 #else
-#include <Wire.h>
+    #include <Wire.h>
 #endif
+
 #include "MCP7940NRTC.h"
+//TODO: REMOVE
+//#include <Arduino.h>
 
 #define MCP7940N_CTRL_ID 0x6F 
 #define MCP7940N_CONF_REG 0x07
@@ -42,7 +21,7 @@ MCP7940NRTC::MCP7940NRTC(uint8_t sdaPin, uint8_t sclPin)
 // Aquire data from buffer and convert to time_t
 time_t MCP7940NRTC::get() {
     tmElements_t tm;
-    if (read(tm) == false) return 0;
+    if (!read(tm)) return 0;
     return(makeTime(tm));
 }
 
@@ -56,39 +35,39 @@ bool MCP7940NRTC::set(time_t t) {
 bool MCP7940NRTC::read(tmElements_t &tm) {
     uint8_t sec;
     Wire.beginTransmission(MCP7940N_CTRL_ID);
-  #if ARDUINO >= 100  
-    Wire.write((uint8_t)0x00); 
-  #else
-    Wire.send(0x00);
-  #endif  
-    if (Wire.endTransmission() != 0) {
-      _exists = false;
-      return false;
-    }
-    _exists = true;
+    #if ARDUINO >= 100  
+        Wire.write((uint8_t)0x00); 
+    #else
+        Wire.send(0x00);
+    #endif  
+      if (Wire.endTransmission() != 0) {
+          _exists = false;
+          return false;
+      }
+      _exists = true;
 
     // request the 7 data fields   (secs, min, hr, dow, date, mth, yr)
     Wire.requestFrom(MCP7940N_CTRL_ID, tmNbrFields);
     if (Wire.available() < tmNbrFields) return false;
-  #if ARDUINO >= 100
-    sec = Wire.read();
-    tm.Second = bcd2dec(sec & 0x7f);    
-    tm.Minute = bcd2dec(Wire.read() );
-    tm.Hour =   bcd2dec(Wire.read() & 0x3f);  // mask assumes 24hr clock
-    tm.Wday = bcd2dec(Wire.read() );
-    tm.Day = bcd2dec(Wire.read() );
-    tm.Month = bcd2dec(Wire.read() );
-    tm.Year = y2kYearToTm((bcd2dec(Wire.read())));
-  #else
-    sec = Wire.receive();
-    tm.Second = bcd2dec(sec & 0x7f);   
-    tm.Minute = bcd2dec(Wire.receive() );
-    tm.Hour =   bcd2dec(Wire.receive() & 0x3f);  // mask assumes 24hr clock
-    tm.Wday = bcd2dec(Wire.receive() );
-    tm.Day = bcd2dec(Wire.receive() );
-    tm.Month = bcd2dec(Wire.receive() );
-    tm.Year = y2kYearToTm((bcd2dec(Wire.receive())));
-  #endif
+    #if ARDUINO >= 100
+        sec = Wire.read();
+        tm.Second = bcd2dec(sec & 0x7f);    
+        tm.Minute = bcd2dec(Wire.read() );
+        tm.Hour =   bcd2dec(Wire.read() & 0x3f);  // mask assumes 24hr clock
+        tm.Wday = bcd2dec(Wire.read() );
+        tm.Day = bcd2dec(Wire.read() );
+        tm.Month = bcd2dec(Wire.read() );
+        tm.Year = y2kYearToTm((bcd2dec(Wire.read())));
+    #else
+        sec = Wire.receive();
+        tm.Second = bcd2dec(sec & 0x7f);   
+        tm.Minute = bcd2dec(Wire.receive() );
+        tm.Hour =   bcd2dec(Wire.receive() & 0x3f);  // mask assumes 24hr clock
+        tm.Wday = bcd2dec(Wire.receive() );
+        tm.Day = bcd2dec(Wire.receive() );
+        tm.Month = bcd2dec(Wire.receive() );
+        tm.Year = y2kYearToTm((bcd2dec(Wire.receive())));
+    #endif
     if (sec & 0x00) return false; // clock is halted
     return true;
 }
@@ -98,57 +77,74 @@ bool MCP7940NRTC::write(tmElements_t &tm) {
     // stop the clock before writing the values,
     // then restart it after.
     Wire.beginTransmission(MCP7940N_CTRL_ID);
-  #if ARDUINO >= 100  
-    Wire.write((uint8_t)0x00); // reset register pointer  
-    Wire.write((uint8_t)0x00); // Stop the clock. The seconds will be written last
-    Wire.write(dec2bcd(tm.Minute));
-    Wire.write(dec2bcd(tm.Hour));      // sets 24 hour format
-    Wire.write(dec2bcd(tm.Wday));   
-    Wire.write(dec2bcd(tm.Day));
-    Wire.write(dec2bcd(tm.Month));
-    Wire.write(dec2bcd(tmYearToY2k(tm.Year))); 
-  #else  
-    Wire.send(0x00); // reset register pointer  
-    Wire.send(0x00); // Stop the clock. The seconds will be written last
-    Wire.send(dec2bcd(tm.Minute));
-    Wire.send(dec2bcd(tm.Hour));      // sets 24 hour format
-    Wire.send(dec2bcd(tm.Wday));   
-    Wire.send(dec2bcd(tm.Day));
-    Wire.send(dec2bcd(tm.Month));
-    Wire.send(dec2bcd(tmYearToY2k(tm.Year)));   
-  #endif
+    #if ARDUINO >= 100  
+        Wire.write((uint8_t)0x00); // reset register pointer  
+        Wire.write((uint8_t)0x00); // Stop the clock. The seconds will be written last
+        Wire.write(dec2bcd(tm.Minute));
+        Wire.write(dec2bcd(tm.Hour));      // sets 24 hour format
+        Wire.write(dec2bcd(tm.Wday));   
+        Wire.write(dec2bcd(tm.Day));
+        Wire.write(dec2bcd(tm.Month));
+        Wire.write(dec2bcd(tmYearToY2k(tm.Year))); 
+    #else  
+        Wire.send(0x00); // reset register pointer  
+        Wire.send(0x00); // Stop the clock. The seconds will be written last
+        Wire.send(dec2bcd(tm.Minute));
+        Wire.send(dec2bcd(tm.Hour));      // sets 24 hour format
+        Wire.send(dec2bcd(tm.Wday));   
+        Wire.send(dec2bcd(tm.Day));
+        Wire.send(dec2bcd(tm.Month));
+        Wire.send(dec2bcd(tmYearToY2k(tm.Year)));   
+    #endif
     if (Wire.endTransmission() != 0) {
-      _exists = false;
-      return false;
+        _exists = false;
+        return false;
     }
     _exists = true;
 
     // Now go back and set the seconds, starting the clock back up as a side effect
     Wire.beginTransmission(MCP7940N_CTRL_ID);
-  #if ARDUINO >= 100  
-    Wire.write((uint8_t)0x00); // reset register pointer  
-    Wire.write(dec2bcd(tm.Second) | 0x80); // write the seconds and start oscillator
-  #else  
-    Wire.send(0x00); // reset register pointer  
-    Wire.send(dec2bcd(tm.Second) | 0x80); // write the seconds and start oscillator
-  #endif
+    #if ARDUINO >= 100  
+        Wire.write((uint8_t)0x00); // reset register pointer  
+        Wire.write(dec2bcd(tm.Second) | 0x80); // write the seconds and start oscillator
+    #else  
+        Wire.send(0x00); // reset register pointer  
+        Wire.send(dec2bcd(tm.Second) | 0x80); // write the seconds and start oscillator
+    #endif
     if (Wire.endTransmission() != 0) {
-      _exists = false;
-      return false;
+        _exists = false;
+        return false;
     }
     _exists = true;
     return true;
 }
 
-//TODO: Why does this return unsigned char and not bool??
 //Makes use of OSCRUN bit which is bit 6 of 0x03 register.
-unsigned char MCP7940NRTC::isRunning() {
-    Wire.beginTransmission(MCP7940N_CTRL_ID);
-  #if ARDUINO >= 100  
-    Wire.write((uint8_t)0x03); 
-  #else
-    Wire.send(0x03);
-  #endif  
+//OSCRUN 1 = Oscillator is enabled and running
+//OSCRUN 0 = Oscillator has stopped or has been disabled
+bool MCP7940NRTC::isRunning() {
+    // const uint8_t cuarenta = 0x40;
+    uint8_t dataR = getRegister(0x03);
+    // Serial.print("recv: ");
+    // Serial.println(dataR,BIN);
+    // // if ((dataR & cuarenta) == cuarenta)
+    // //     Serial.println("TRUE");
+    // Serial.println(uint8_t(dataR) & (1<<6));
+    // Serial.println(dataR & (1<<6) == (1<<6));
+    // // return (dataR & cuarenta);
+    // if ((dataR >> 6) & 1)
+    //     Serial.print("tru");
+    // else
+    //     Serial.print("fals");
+    //return ((dataR & (1<<6)) == (1<<6));
+    return (dataR >> 6) & 1;
+}
+    /*Wire.beginTransmission(MCP7940N_CTRL_ID);
+    #if ARDUINO >= 100  
+      Wire.write((uint8_t)0x03); 
+    #else
+      Wire.send(0x03);
+    #endif  
     Wire.endTransmission();
 
     // Just fetch the seconds register and check the top bit
@@ -160,18 +156,17 @@ unsigned char MCP7940NRTC::isRunning() {
     // Serial.println(readB & 0x80);
     // Serial.print("& 0x80 == 0x80: ");
     // Serial.println((readB & 0x80) == 0x80);
-  #if ARDUINO >= 100
-    return (Wire.read() & 0x80);
-  #else
-    return (Wire.receive() & 0x80);
-  #endif  
-}
+    #if ARDUINO >= 100
+      return (Wire.read() & 0x80);
+    #else
+      return (Wire.receive() & 0x80);
+    #endif  */
 
 void MCP7940NRTC::setConfig(const uint8_t confValue) {
     setRegister(MCP7940N_CONF_REG, confValue);
 }
 
-uint8_t MCP7940NRTC::getConfig() {
+uint8_t MCP7940NRTC::getConfig() const {
     return getRegister(MCP7940N_CONF_REG);
 }
 
@@ -211,42 +206,42 @@ uint8_t MCP7940NRTC::getConfig() {
 
 // PRIVATE FUNCTIONS
 
-uint8_t MCP7940NRTC::getRegister(const uint8_t regAddr) {
+uint8_t MCP7940NRTC::getRegister(const uint8_t regAddr) const {
     Wire.beginTransmission(MCP7940N_CTRL_ID);
-  #if ARDUINO >= 100  
-    Wire.write((uint8_t)regAddr); 
-  #else
-    Wire.send(regAddr);
-  #endif  
+    #if ARDUINO >= 100  
+        Wire.write((uint8_t)regAddr); 
+    #else
+        Wire.send(regAddr);
+    #endif  
     Wire.endTransmission();
 
     Wire.requestFrom(MCP7940N_CTRL_ID, 1);
-  #if ARDUINO >= 100
-    return Wire.read();
-  #else
-    return Wire.receive();
-  #endif
+    #if ARDUINO >= 100
+        return Wire.read();
+    #else
+        return Wire.receive();
+    #endif
 }
 
 void MCP7940NRTC::setRegister(const uint8_t regAddr, const uint8_t regData) {
     Wire.beginTransmission(MCP7940N_CTRL_ID);
-  #if ARDUINO >= 100  
-    Wire.write((uint8_t)regAddr); // Point to calibration register
-    Wire.write(regData);
-  #else  
-    Wire.send(regAddr); // Point to calibration register
-    Wire.send(regData);
-  #endif
+    #if ARDUINO >= 100  
+        Wire.write((uint8_t)regAddr); // Point to calibration register
+        Wire.write(regData);
+    #else  
+        Wire.send(regAddr); // Point to calibration register
+        Wire.send(regData);
+    #endif
     Wire.endTransmission();  
 }
 
 // Convert Decimal to Binary Coded Decimal (BCD)
 uint8_t MCP7940NRTC::dec2bcd(uint8_t num) {
-  return ((num/10 * 16) + (num % 10));
+    return ((num/10 * 16) + (num % 10));
 }
 
 // Convert Binary Coded Decimal (BCD) to Decimal
 uint8_t MCP7940NRTC::bcd2dec(uint8_t num) {
-  return ((num/16 * 10) + (num % 16));
+    return ((num/16 * 10) + (num % 16));
 }
 
--- MCP7940NRTC.h
+++ MCP7940NRTC.h
@@ -1,8 +1,23 @@
-/*
- * MCP7940NRTC.h - library for MCP7940N RTC
- * This library is intended to be uses with Arduino Time library functions
- * Based on the library for the DS1307 https://github.com/PaulStoffregen/DS1307RTC
- */
+// #############################################################################
+// #
+// # Name       : MCP7940N RTC Library
+// # Author     : Juan L. Perez Diez <ender.vs.melkor at gmail>
+//
+// # Based on the library for the DS1307 https://github.com/PaulStoffregen/DS1307RTC
+//
+// #  This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #ifndef MCP7940NRTC_h
 #define MCP7940NRTC_h
@@ -18,16 +33,16 @@ class MCP7940NRTC {
     bool read(tmElements_t &tm);
     bool write(tmElements_t &tm);
     bool chipPresent() { return _exists; }
-    unsigned char isRunning();
+    bool isRunning();
     void setConfig(uint8_t confValue);
-    uint8_t getConfig();
-    void setCalibration(char calValue);
-    char getCalibration();
+    uint8_t getConfig() const;
+    //void setCalibration(char calValue);
+    //char getCalibration();
 
   private:
     uint8_t _sdaPin, _sclPin;
     bool _exists;
-    uint8_t getRegister(const uint8_t regAddr);
+    uint8_t getRegister(const uint8_t regAddr) const;
     void setRegister(const uint8_t regAddr, const uint8_t regData);
     static uint8_t dec2bcd(uint8_t num);
     static uint8_t bcd2dec(uint8_t num);
